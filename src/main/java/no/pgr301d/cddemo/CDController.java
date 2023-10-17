d1084b63aedc7b9722e5c65be0f1298fb509f82a
jalvsaker <ec2-user@ip-172-31-40-157.eu-west-1.compute.internal>
2023-10-17 07:03:33 +0000

    Change

--- src/main/java/no/pgr301d/cddemo/CDController.java
+++ src/main/java/no/pgr301d/cddemo/CDController.java
@@ -3,13 +3,14 @@ package no.pgr301d.cddemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 class CDController {
 
     @GetMapping(path = "/")
     public String hello() {
-        return "Hello world! at " +  LocalDateTime.now().toString();
+        return "Hello world! at " +  LocalDateTime.now().toString() + " " + UUID.randomUUID().toString();;
     }
 
 }
